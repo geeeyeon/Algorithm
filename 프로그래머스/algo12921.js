d52b9327c7ea92ff270c1074ec1ae55425a2c8ac
geeeyeon <parafunction@hanmail.net>
2021-11-09 23:43:10 +0900

    소수찾기*

---
+++ 프로그래머스/algo12921.js
@@ -0,0 +1,27 @@
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/12921
+ * 소수 찾기
+ * 1부터 입력받은 N 사이에 있는 소수의 개수 구하기
+ *
+ */
+
+function solution(n) {
+  let answer = 0;
+
+  for (let i = 2; i <= n; i++) {
+    console.log(i, "->", isPrime(i));
+    if (isPrime(i)) answer++;
+  }
+  return answer;
+}
+
+function isPrime(n) {
+  // console.log("%%%", parseInt(Math.sqrt(n)));
+  if (n === 2) return true;
+  for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+    if (n % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
