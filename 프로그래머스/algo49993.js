d2eeda2743d9a16e4c06101e63bdd4ec25e5f3c9
geeeyeon <parafunction@hanmail.net>
2021-12-01 01:39:32 +0900

    다시풀기 ***

--- 프로그래머스/algo49993.js
+++ 프로그래머스/algo49993.js
@@ -6,25 +6,22 @@
 
 function solution(skill, skill_trees) {
   let answer = 0;
-  let step = skill.split("");
-  let tmp = new Array(skill_trees.length).fill(-1);
 
-  for (let i = 0; i < step.length; i++) {
-    skill_trees.map((v, i) => {
-      let idx = v.indexOf(step[i]);
-      if (idx > tmp[i]) tmp[i] = idx;
-      else if (idx < tmp[i]) tmp[i] = 100;
-      else if (idx === -1) tmp[i] = -1;
-    });
-  }
-  console.log(tmp);
-
-  tmp.map((v) => {
-    if (v >= 0) {
-      answer++;
-    }
-  });
   return answer;
 }
 
-console.log("--->", solution("CBD", ["C", "D", "CB", "BDA"]));
+console.log(
+  "--->",
+  solution("CBDK", [
+    "CB",
+    "CXYB",
+    "BD",
+    "AECD",
+    "ABC",
+    "AEX",
+    "CDB",
+    "CBKD",
+    "IJCB",
+    "LMDK",
+  ])
+);
