9b541d65db5d03d8bf2d00de37634103b0e71612
geeeyeon <parafunction@hanmail.net>
2021-12-25 00:34:16 +0900

    스킬트리

--- 프로그래머스/algo49993.js
+++ 프로그래머스/algo49993.js
@@ -6,6 +6,22 @@
 
 function solution(skill, skill_trees) {
   let answer = 0;
+  let step = skill.split("");
+  let filter_skills = [];
+  for (let i = 0; i < skill_trees.length; i++) {
+    let target = skill_trees[i].split("");
+    let tmp = [];
+    target.filter((v) => {
+      step.includes(v) ? tmp.push(v) : null;
+    });
+    filter_skills.push(tmp);
+  }
+  filter_skills.forEach((val, i) => {
+    for (let j = 0; j < val.length; j++) {
+      if (val[j] !== step[j]) return;
+    }
+    answer++;
+  });
 
   return answer;
 }
@@ -24,4 +40,6 @@ console.log(
     "IJCB",
     "LMDK",
   ])
-);
+); //4
+console.log("--->", solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])); //2
+console.log("--->", solution("BDC", ["AAAABACA"])); //0
