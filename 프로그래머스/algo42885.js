19531456a5f5ac609cd32ccb1ffbad5e1fcfab23
geeeyeon <parafunction@hanmail.net>
2022-01-07 15:26:14 +0900

    구명보트

---
+++ 프로그래머스/algo42885.js
@@ -0,0 +1,25 @@
+/**
+ * https://programmers.co.kr/learn/courses/30/lessons/42885
+ * 구명보트
+ */
+
+function solution(people, limit) {
+  let answer = 0;
+  people.sort((a, b) => b - a);
+  let leftPoint = 0;
+  let rightPoint = people.length - 1;
+  while (leftPoint <= rightPoint) {
+    if (people[leftPoint] + people[rightPoint] <= limit) {
+      leftPoint++;
+      rightPoint--;
+    } else {
+      leftPoint++;
+    }
+    answer++;
+  }
+  return answer;
+}
+
+console.log("--->", solution([70, 50, 80, 50], 100)); //3
+console.log("--->", solution([70, 80, 50], 100)); //3
+console.log("--->", solution([40, 50, 150, 160], 200)); //2
