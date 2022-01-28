8168bd79bc70ed4b909f97c6789a4f2cc2c06cf8
geeeyeon <parafunction@hanmail.net>
2022-01-28 18:21:44 +0900

    소수찾기 다시풀었음

--- 프로그래머스/algo12921.js
+++ 프로그래머스/algo12921.js
@@ -2,26 +2,37 @@
  * https://programmers.co.kr/learn/courses/30/lessons/12921
  * 소수 찾기
  * 1부터 입력받은 N 사이에 있는 소수의 개수 구하기
- * *****효율성에서 탈락.. 다시풀기
  */
 
 function solution(n) {
-  let answer = 0;
-
-  for (let i = 2; i <= n; i++) {
-    console.log(i, "->", isPrime(i));
-    if (isPrime(i)) answer++;
-  }
-  return answer;
-}
-
-function isPrime(n) {
-  // console.log("%%%", parseInt(Math.sqrt(n)));
-  if (n === 2) return true;
-  for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
-    if (n % i === 0) {
-      return false;
+  let arr = [false, false, ...new Array(n - 1).fill(true)];
+  for (let i = 2; i * i <= n; i++) {
+    if (arr[i]) {
+      for (let j = i * 2; j <= n; j += i) {
+        arr[j] = false;
+      }
     }
   }
-  return true;
+  return arr.filter((ele) => ele === true).length;
 }
+
+// function solution(n) {
+//   let answer = 0;
+
+//   for (let i = 2; i <= n; i++) {
+//     console.log(i, "->", isPrime(i));
+//     if (isPrime(i)) answer++;
+//   }
+//   return answer;
+// }
+
+// function isPrime(n) {
+//   // console.log("%%%", parseInt(Math.sqrt(n)));
+//   if (n === 2) return true;
+//   for (let i = 2; i <= parseInt(Math.sqrt(n)); i++) {
+//     if (n % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
