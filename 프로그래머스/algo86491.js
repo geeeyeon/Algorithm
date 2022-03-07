ee10bfe40f6ee99367ebab32977a3c034dc8f695
geeeyeon <parafunction@hanmail.net>
2022-03-07 23:03:22 +0900

    수정 : 최조직사각형 다시풀기

--- 프로그래머스/algo86491.js
+++ 프로그래머스/algo86491.js
@@ -10,20 +10,49 @@
 });
  */
 
- function solution(sizes){
-    let maxL=0;
-    let maxR=0;
-    let tmp = [];
-    
-    sizes.map((v)=>{
-       v[0]>v[1] ? tmp.push([v[0],v[1]]) : tmp.push([v[1],v[0]]);
-    })
-    
-    for(let j=0; j<sizes.length; j++){
-        maxL = tmp[j][0] > maxL ? tmp[j][0]:maxL;
-        maxR = tmp[j][1] > maxR ? tmp[j][1]:maxR;
-    }
+// function solution1(sizes) {
+//   let maxL = 0;
+//   let maxR = 0;
+//   let tmp = [];
 
-    return maxL*maxR;
+//   sizes.map((v) => {
+//     v[0] > v[1] ? tmp.push([v[0], v[1]]) : tmp.push([v[1], v[0]]);
+//   });
+
+//   for (let j = 0; j < sizes.length; j++) {
+//     maxL = tmp[j][0] > maxL ? tmp[j][0] : maxL;
+//     maxR = tmp[j][1] > maxR ? tmp[j][1] : maxR;
+//   }
+
+//   return maxL * maxR;
+// }
+
+function solution(sizes) {
+  let tmp = [0, 0]; //min,max
+  sizes.map((v) => {
+    tmp[0] = tmp[0] < Math.min(...v) ? Math.min(...v) : tmp[0];
+    tmp[1] = tmp[1] < Math.max(...v) ? Math.max(...v) : tmp[1];
+  });
+
+  return tmp[0] * tmp[1];
 }
-console.log('--->',solution([[60, 50], [30, 70], [60, 30], [80, 40]]));
+
+console.log(
+  "--->",
+  solution([
+    [60, 50],
+    [30, 70],
+    [60, 30],
+    [80, 40],
+  ])
+); //4000
+console.log(
+  "--->",
+  solution([
+    [10, 7],
+    [12, 3],
+    [8, 15],
+    [14, 7],
+    [5, 15],
+  ])
+); //120
