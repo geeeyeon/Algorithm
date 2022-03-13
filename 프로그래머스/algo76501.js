b47f5df09701da034ecca57aff56b66dba9a0688
geeeyeon <parafunction@hanmail.net>
2022-03-13 23:22:29 +0900

    수정 : 음양더하기 다시풀이

--- 프로그래머스/algo76501.js
+++ 프로그래머스/algo76501.js
@@ -4,13 +4,21 @@
  * *reduce함수 사용해보기
  */
 
-function solution(absolutes, signs) {
+function solution1(absolutes, signs) {
   let answer = 0;
   for (let i = 0; i < absolutes.length; i++) {
     signs[i] ? (answer += absolutes[i]) : (answer -= absolutes[i]);
   }
 
   return answer;
+}
+
+// reduce 함수 사용해서 다시 풀이
+function solution(absolutes, signs) {
+  return absolutes.reduce(
+    (previous, current, index) => previous + current * (signs[index] ? 1 : -1),
+    0
+  );
 }
 
 console.log("--->", solution([4, 7, 12], [true, false, true])); //9
